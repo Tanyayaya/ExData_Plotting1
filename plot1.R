e2633afd117632e4f7b67c25d6603a5f2ff236d5
Tanyayaya <31395377+Tanyayaya@users.noreply.github.com>
2017-09-23 16:47:00 +0800

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,16 @@
+png("plot1.png")
+setwd("E:/R/cousera")
+data<-read.table("household_power_consumption/household_power_consumption.txt",
+                 sep=";",header=FALSE,col.names=c("date","time",
+                 "global_active_power","global_reactive_power",
+                 "voltage","global_intensity","sub_metering_1",
+                 "sub_metering_2","sub_metering_3"),stringsAsFactors = F)
+data$date<-as.Date(data$date,"%d/%m/%Y")
+startDate = as.Date("2007-02-01");
+endDate = as.Date("2007-02-02");
+data<-subset(data,date==startDate|date==endDate)
+data<-subset(data,global_active_power!="?")
+with(data,hist(as.numeric(global_active_power),12,
+    col="red",main="Global Active Power",xlab = "Global Active Power(kilowatts)",
+    ylab = "Frequency"))
+dev.off()
